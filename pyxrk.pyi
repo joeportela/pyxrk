c477075370b4858016171a482f620f577423cd5a
Joe Portela <jportela14@gmail.com>
2023-06-13 22:41:38 -0400

    typing and linting

--- pyxrk.pyi
+++ pyxrk.pyi
@@ -1,4 +1,12 @@
+import datetime
+
 class Run:
     lap_count: int
+    championship: str
+    track: str
+    venue_type: str
+    vehicle: str
+    racer: str
+    datetime: datetime.datetime
 
 def load_run(path: str) -> Run: ...
