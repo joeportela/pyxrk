da1da746c8782f2f3bd3b3968f003f531114f69e
Joe Portela <jportela14@gmail.com>
2023-06-27 22:21:23 -0400

    test

--- test/test_run.py
+++ test/test_run.py
@@ -1,3 +1,5 @@
+import pyarrow  # type: ignore
+
 from pyxrk import Run
 
 TEST_FILE = "./test/data/test.xrk"
@@ -6,9 +8,9 @@ TEST_FILE = "./test/data/test.xrk"
 def test_lap_table() -> None:
     run = Run.load(TEST_FILE)
     lap = run.get_lap(3)
-    lap.to_table()
+    assert isinstance(lap.to_table(), pyarrow.Table)
 
 
 def test_run_table() -> None:
     run = Run.load(TEST_FILE)
-    run.to_table()
+    assert isinstance(run.to_table(), pyarrow.Table)
